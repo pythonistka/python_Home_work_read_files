d23f6c4b6ef64749b5e8de24d749c651550fb986
Lopatina Darya <baboleh@rambler.ru>
2022-04-04 01:13:44 +0300

    Добавлен gitignore, файл для обработки и функция, для считывания информации из файла

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 
+file_name = "recipes.txt"
+
+def file_worker(file_name: str):
+    with open(file_name, encoding="utf-8") as file:
+        for line in file:
+            print(line.strip())
+
+result = file_worker(file_name)
